e467b857825a9f0f1350b8446f14a8342f8053cb
Lars Kue <lars@kuehmichel.de>
2018-03-27 21:17:35 +0200

    added basic draw functionality

---
+++ player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lars on 27/03/2018.
+//
+
+#include "player.h"
---
+++ player.h
@@ -0,0 +1,14 @@
+//
+// Created by Lars on 27/03/2018.
+//
+
+#ifndef SIDESCROLLER_PLAYER_H
+#define SIDESCROLLER_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //SIDESCROLLER_PLAYER_H
